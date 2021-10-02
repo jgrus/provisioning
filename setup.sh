c06f0487d372a2f9b7065e74f7dd1a6450af412a
Johan Sand <johanpsand@pm.me>
2021-10-02 17:58:26 +0200

    Correts invocation of tar to extract the right file to the right location

--- setup.sh
+++ setup.sh
@@ -9,10 +9,9 @@ install_nebula_arm() {
 echo Downloading and setting up temporary access via Nebula 
 mkdir /opt/nebula
 wget --directory-prefix=/opt/nebula/ https://github.com/slackhq/nebula/releases/download/v1.4.0/nebula-linux-arm64.tar.gz
-tar -xzf nebula-linux-arm64.tar.gz
+tar -xzf /opt/nebulanebula-linux-arm64.tar.gz -C /opt/nebula/
 chmod +x nebula
 
-setup_temporary_access()
 }
 
 install_nebula_amd64() {
@@ -20,10 +19,9 @@ install_nebula_amd64() {
 echo Downloading and setting up temporary access via Nebula 
 mkdir /opt/nebula
 wget --directory-prefix=/opt/nebula/ https://github.com/slackhq/nebula/releases/download/v1.4.0/nebula-linux-amd64.tar.gz
-tar -xzf nebula-linux-amd64.tar.gz
+tar -xzf /opt/nebula/nebula-linux-amd64.tar.gz -C /opt/nebula
 chmod +x nebula
 
-setup_temporary_access()
 }
 
 setup_temporary_access() {
@@ -58,6 +56,7 @@ esac
 
 user=$(whoami)
 echo Setting up remote administration on $user@10.11.11.11
+setup_temporary_access()
 start_nebula()
 exit 0
 
