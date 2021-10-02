603b0bcd8a1c02589a4cc90ab4f6d93f097ea496
Johan Sand <johanpsand@pm.me>
2021-10-02 17:44:40 +0200

    Reverses the order of functions vs global script in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,9 @@
 #!/bin/bash -e
 
-echo Installing ssh server
-apt install openssh-server
-echo Importing SSH keys from github profile
-ssh-import-id gh:jgrus
 
-
-echo Checking platform architecture
-arch=$(arch)
-case $arch in
-	aarch64) install_nebula_arm() ;;
-	x86_64) install_nebula_amd64() ;;
-	* ) echo Unkown architecture $arch. Manual configuration is required, exiting setup script. && exit 1
-esac
-
-user=$(whoami)
-echo Setting up remote administration on $user@10.11.11.11
-start_nebula()
-exit 0
-
-
-
+#############################################################################################################
+#DEFINING FUNCTIONS
+#############################################################################################################
 install_nebula_arm() {
 
 echo Downloading and setting up temporary access via Nebula 
@@ -56,3 +39,28 @@ start_nebula() {
 echo Starting nebula server to enable remote access
 /opt/nebula/nebula -config /opt/nebula/config.yml
 }
+
+
+###################################################################################################################
+#>RUNNING SCRIPT
+###################################################################################################################
+echo Installing ssh server
+apt install openssh-server
+echo Importing SSH keys from github profile
+ssh-import-id gh:jgrus
+
+
+echo Checking platform architecture
+arch=$(arch)
+case "$arch" in
+	'aarch64'|'amd64' ) install_nebula_arm() ;;
+	'x86_64' ) install_nebula_amd64() ;;
+	* ) echo Unkown architecture $arch. Manual configuration is required, exiting setup script. && exit 1 ;;
+esac
+
+user=$(whoami)
+echo Setting up remote administration on $user@10.11.11.11
+start_nebula()
+exit 0
+
+
